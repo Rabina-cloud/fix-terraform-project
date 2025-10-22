6137539683e36a143e2536f8973a61a54096e82a
rabina <rabinay03@gmail.com>
2025-10-21 22:11:56 -0400

    Codebuild-terraform

--- terraform-source/main.tf
+++ terraform-source/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "webserver" {
   instance_type          = "t2.micro"
   key_name               = var.key_name
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  vpc_security_group_ids = [aws_security_group.my_securitygp.id]
 
   tags = {
     Name = "${var.environment}-server"
